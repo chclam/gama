6f32861d1e99e2102329057b4a882c03392bce05
Chris Lam <chris@chrislam.nl>
2022-09-15 19:25:24 +0200

    fixed the issue where the multiprocessing library couldn't serialize fasttext

--- gama/configuration/fasttextclassifier.py
+++ gama/configuration/fasttextclassifier.py
@@ -6,33 +6,34 @@ import openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from datetime import datetime
-from sklearn.base import ClassifierMixin
+from sklearn.base import ClassifierMixin, BaseEstimator
 
-class FastTextClassifier(ClassifierMixin):
+class FastTextClassifier(BaseEstimator, ClassifierMixin):
   def __init__(self):
     self._estimator_type = "classifier"
-    self.model = None
     self.classes_ = None
     self.num_labels = None
+    self.model_filename = None
     
-  def fit(self, X, y, save_model=False):
+  def fit(self, X, y):
     data_fn = "cache/test_data.txt"
     data = self.preprocess(X, y)
     self.classes_ = sorted(list(pd.Series(y).unique()))
     pd.set_option('display.max_colwidth', None)
     with open(data_fn, "w+") as out:
       out.write(data.to_string(index=False))
-    self.model = fasttext.train_supervised(data_fn)
-    if save_model:
-      self.model.save_model(f"cache/ft_model_{datetime.now()}.bin")
+    model = fasttext.train_supervised(data_fn)
+    self.model_filename = f"cache/ft_model_{datetime.now()}.bin"
+    model.save_model(self.model_filename)
 
   def predict(self, X, ret_proba=False):
-    if self.model is None:
+    if self.model_filename is None:
       raise Exception("Model is not fitted yet. Please fit the model first.")
     pd.set_option('display.max_colwidth', None)
     data = self.preprocess(X).to_string(index=False)
     data = data.split("\n") # split the data into an array of rows
-    labels_pred, probs_pred = self.model.predict(data, k=len(self.classes_) if ret_proba else 1)
+    model = fasttext.load_model(self.model_filename)
+    labels_pred, probs_pred = model.predict(data, k=len(self.classes_) if ret_proba else 1)
     # sort the probabilities by label order
     ret = []
     for row_labels, row_probs in zip(labels_pred, probs_pred):
@@ -47,37 +48,21 @@ class FastTextClassifier(ClassifierMixin):
     pass
 
   def validate(self, X, y):
-    if self.model is None:
+    # refactor
+    if self.model_filename is None:
       raise Exception("Model is not trained. Please train the model using 'fit' before validating the model.")
     data = self.preprocess(X, y)
     val_data_name = f"cache/validation_set_{datetime.now()}"
     with open(val_data_name, 'w') as out:
       out.write(data.to_string(index=False))
-    print(self.model.test(val_data_name, k=5))
+    model = fasttext.load_model(self.model_filename)
+    # get rid of this
+    print(model.test(val_data_name, k=5))
 
   def preprocess(self, X, y=None):
     X = pd.DataFrame(X).reset_index(drop=True)
-    y = pd.DataFrame(y).reset_index(drop=True)
-    print("X index", X.index)
-    print("y index", y.index)
-#    if not isinstance(X, pd.DataFrame) or not (isinstance(y, pd.DataFrame) or isinstance(y, pd.Series)):
-#      X = pd.DataFrame(X)
-#      y = pd.DataFrame(y)
-#    if not isinstance(X, pd.DataFrame):
-#      if isinstance(X, pd.Series):
-#        X = pd.DataFrame(X, index=X.index)
-#      elif isinstance(X, np.ndarray):
-#        X = pd.DataFrame(X)
-#      else:
-#        raise ValueError("Unsupported type for X.")
-#
-#    if not isinstance(y, pd.DataFrame):
-#      if isinstance(y, pd.Series):
-#        y = pd.DataFrame(y, index=y.index)
-#      elif isinstance(y, np.ndarray): 
-#        y = pd.DataFrame(y)
-#      else:
-#        raise ValueError("Unsupported type for y.")
+    if y is not None:
+      y = pd.DataFrame(y).reset_index(drop=True)
 
     # formatting y to fit fasttext expected format
     ret = X.copy()
@@ -107,6 +92,6 @@ if __name__ == "__main__":
 
   clf = FastTextClassifier()
   clf.fit(X_train, y_train)
-  #print(clf.validate(X_test, y_test))
+ #print(clf.validate(X_test, y_test))
   print(clf.predict_proba(X_test))
   #import pdb; pdb.set_trace()
