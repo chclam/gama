caf621cf03588ef52f96d1db274f60545d0a1fd4
Chris Lam <chris@chrislam.nl>
2022-10-31 13:08:30 +0100

    add fasttext test bench

--- ft_test.py
+++ ft_test.py
@@ -6,31 +6,27 @@ import pandas as pd
 import openml
 import json
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score, cross_validate
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import LabelEncoder
-from gama.configuration.fasttextclassifier import FastTextClassifier
-from gama import GamaClassifier
-from gama.search_methods.asha import AsynchronousSuccessiveHalving
 from sklearn.metrics import roc_auc_score, make_scorer, roc_curve
 from gama.postprocessing.ensemble import EnsemblePostProcessing
+from gama.configuration.fasttextclassifier import FastTextClassifier
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_auc_score
-from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import accuracy_score
 from sklearn.dummy import DummyClassifier
 
-
-def fasttext_run(X, y, pretrainedVectors="", dim=100):
+def fasttext_run(X, y, cv, pretrainedVectors="", dim=100):
   # Only keep the columns with string values
-  X = X[[col_name for col_name in X.columns if X[col_name].dtype == np.dtype('O')]]
+  # X = X[[col_name for col_name in X.columns if X[col_name].dtype == np.dtype('O')]]
 
   if isinstance(y, np.ndarray):
     y = pd.Series(LabelEncoder().fit_transform(y))
   else:
     y = pd.Series(LabelEncoder().fit_transform(y), index=y.index)
 
-  clf = FastTextClassifier(minn=0, maxn=0, epoch=10, lr=0.1, pretrainedVectors=pretrainedVectors, dim=dim)
+  clf = FastTextClassifier(minn=0, maxn=0, epoch=5, lr=0.1, pretrainedVectors=pretrainedVectors, dim=dim)
 
   if len(np.unique(y)) < 2:
     raise ValueError("y_true has fewer than 2 unique values.")
@@ -42,14 +38,14 @@ def fasttext_run(X, y, pretrainedVectors="", dim=100):
   scores = cross_val_score(clf,
     X,
     y, 
-    cv=10,
+    cv=cv,
     scoring=scorer, 
-    n_jobs=3 if pretrainedVectors == "" else 1
+    n_jobs=-1
   )
   
   # fill the nannies 
   if any(np.isnan(scores)):
-    dum_score = cross_val_score(DummyClassifier(), X, y, cv=10, scoring=scorer)
+    dum_score = cross_val_score(DummyClassifier(), X, y, cv=5, scoring=scorer)
     scores = np.nan_to_num(scores, nan=np.nanmean(dum_score))
 
   print(f"{scorer}:", scores)
@@ -63,11 +59,12 @@ def log_error(e):
   print(e)
 
 def main(ids):
-  for ds_name, d_id in ids.items():
+  #for ds_name, d_id in ids.items():
+  for d_id in ids:
       
     dataset_scores = {
       "data_id": d_id,
-      "data_name": ds_name 
+      #"data_name": ds_name 
     }
 
     try:
@@ -84,26 +81,30 @@ def main(ids):
       if len(X) > 100000:
         X, _, y, _ = train_test_split(X, y, stratify=y, train_size=100000, random_state=0, shuffle=True)
 
+      dataset_scores["name"] = dataset.name
       dataset_scores["metric"] = "roc_auc" if len(np.unique(y)) == 2 else "neg_log_loss"
 
     except Exception as e:
       print(f"{d_id} openml failed: {e}\n")
       log_error(e)
+      continue
+
+    cv = StratifiedKFold(n_splits=5, random_state=None, shuffle=True)
 
     try:
-      dataset_scores["fasttext"] = fasttext_run(X, y)
+      dataset_scores["fasttext"] = fasttext_run(X, y, cv=cv.split(X, y))
     except Exception as e:
       print(f"{d_id} fasttext failed: {e}\n")
       log_error(e)
 
     try:
-      dataset_scores["fasttext_100"] = fasttext_run(X, y, "100.vec", dim=100)
+      dataset_scores["fasttext_100"] = fasttext_run(X, y, cv=cv.split(X, y), pretrainedVectors="100.vec", dim=100)
     except Exception as e:
       print(f"{d_id} fasttext failed: {e}\n")
       log_error(e)
 
     try:
-      if not os.path.isdir("roc_log_results"):
+      if not os.path.isdir(f"roc_log_results"):
         os.mkdir("roc_log_results")
       with open(f"roc_log_results/results_{d_id}_{int(time.time())}.json", "w+") as f:
         json.dump(dataset_scores, f)
@@ -113,14 +114,19 @@ def main(ids):
 if __name__ == "__main__":
   # Getting the data set
 
-  ids = {
-    "beerreviews": 42078,
-    "road_safety": 42803,
-    "traffic_violations": 42132,
-    "drug_directory": 43044,
-    "kickstarter": 42076,
-    "openpayments": 42738,
-  }
+#  ids = {
+#    "beerreviews": 42078,
+#    "road_safety": 42803,
+#    "traffic_violations": 42132,
+#    "drug_directory": 43044,
+#    "kickstarter": 42076,
+#    "openpayments": 42738,
+#  }
+
+  openml.config.apikey = "6582f698e8e48968a7566b87fff8a75e"  # set the OpenML Api Key
+#  benchmark_suite = openml.study.get_suite('OpenML-CC18')  # obtain the benchmark suite
+
+  ids = [3, 6, 11, 12, 14, 15, 16, 18, 22, 23, 28, 29, 31, 32, 37, 43, 49, 53, 3560, 3902, 3904, 3913, 3917, 3918, 42076, 42078, 42132, 42738, 42803, 43044]
 
   for _ in range(1):
     main(ids)
--- gama/configuration/fasttextclassifier.py
+++ gama/configuration/fasttextclassifier.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score 
 from sklearn.base import ClassifierMixin, BaseEstimator
 from time import time
+from sklearn.exceptions import NotFittedError
 
 class FastTextClassifier(BaseEstimator, ClassifierMixin):
   def __init__(self, lr=0.1, epoch=5, wordNgrams=1, minn=0, maxn=0, pretrainedVectors="", dim=100):
@@ -16,8 +17,7 @@ class FastTextClassifier(BaseEstimator, ClassifierMixin):
     self.wordNgrams = wordNgrams
     self.minn = minn
     self.maxn = maxn
-    # defaults pretrainedVectors="" and dim=100 are kept the same as in the FastText library
-    self.pretrainedVectors=pretrainedVectors
+    self.pretrainedVectors=pretrainedVectors # by default dim=100 if pretrainedVectors="" 
     self.dim = dim
     
   def fit(self, X, y, classes=None):
@@ -29,11 +29,9 @@ class FastTextClassifier(BaseEstimator, ClassifierMixin):
     data_fn = f"cache/test_data{time()}.txt"
 
     if self.classes_ is None:
-      self.classes_ = sorted(np.unique(y))
+      self.classes_ = np.unique(y)
     pd.set_option('display.max_colwidth', None) # do this so that .to_string() actually converts all data to string
     data = self.preprocess(X, y, data_fn=data_fn)
-#    with open(data_fn, "w+") as out:
-#      out.write(data.to_string(index=False, header=False))
     model = fasttext.train_supervised(data_fn, lr=self.lr, epoch=self.epoch, wordNgrams=self.wordNgrams, minn=self.minn, maxn=self.maxn, pretrainedVectors=self.pretrainedVectors, dim=self.dim)
     self.model_filename = f"cache/ft_model_{time()}.bin"
     # save and load the model due to issues with multiprocessing when passing on the fit model.
@@ -42,7 +40,7 @@ class FastTextClassifier(BaseEstimator, ClassifierMixin):
 
   def predict(self, X, ret_proba=False):
     if self.model_filename is None:
-      raise Exception("Model is not fitted yet. Please fit the model first.")
+      raise NotFittedError("Model is not fitted yet. Please fit the model first.")
     pd.set_option('display.max_colwidth', None)
     data = self.preprocess(X)
     data = data.split("\n") # split the data into an array of rows
@@ -84,9 +82,6 @@ class FastTextClassifier(BaseEstimator, ClassifierMixin):
       y = y.astype(str).apply(lambda r: "".join(v for v in r.values), axis=1)
       data = pd.concat([data, y], axis=1)
       # Sloppy hack for appending "__label__" faster to all values  
-    '''
-    TODO: write a bash script to make this faster
-    '''
     pd.set_option('display.max_colwidth', None) # do this so that .to_string() actually converts all data to string
     data.to_csv(data_fn, header=None, index=None, sep=' ', mode='w')
     # call bash script for preprocessing
