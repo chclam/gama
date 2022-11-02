c5db017e3cea117b93f40219ad8a023a0c931b3a
Chris Lam <chris@chrislam.nl>
2022-11-02 23:40:23 +0100

    add open-cc18-test.py

---
+++ open-cc18-test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import os
+import json
+import pickle
+import openml
+import pickle
+import numpy as np
+from tqdm import trange
+from gama import GamaClassifier
+from sklearn.model_selection import StratifiedKFold 
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score, log_loss
+from gama.postprocessing import EnsemblePostProcessing 
+
+if __name__ == "__main__":
+  CV_FOL_NAME = "cv_folds"
+  openml.config.apikey = "6582f698e8e48968a7566b87fff8a75e"  # set the OpenML Api Key
+  benchmark_suite = openml.study.get_suite('OpenML-CC18')  # obtain the benchmark suite
+
+  if not os.path.isdir(CV_FOL_NAME):
+    raise BaseException("No CV folds!")
+
+  if not os.path.isdir("open-cc18-gama-ft"):
+    os.mkdir("open-cc18-gama-ft")
+
+  for _, t_id in zip(trange(len(benchmark_suite.tasks)), benchmark_suite.tasks):
+    task = openml.tasks.get_task(t_id)
+    dataset = task.get_dataset()
+    X, y, _, _ = dataset.get_data(
+      target=dataset.default_target_attribute, dataset_format="dataframe"
+    )
+
+    if len(np.unique(y)) == 2:
+      scorer_name = "roc_auc"
+      scorer = roc_auc_score
+    else:
+      scorer_name = "neg_log_loss"
+      scorer = log_loss
+
+    with open(f"{CV_FOL_NAME}/{dataset.id}.pkl", "rb") as f:
+      cv = pickle.load(f) 
+ 
+    for fold_nr, (train_idx, test_idx) in enumerate(cv):
+      X_train, y_train = X.iloc[train_idx], y.iloc[train_idx]
+      X_test, y_test = X.iloc[test_idx], y.iloc[test_idx]
+      
+      clf = GamaClassifier(scoring=scorer_name, max_total_time=180, max_eval_time=120, n_jobs=-1, post_processing=EnsemblePostProcessing())
+      clf.fit(X_train, y_train)
+      y_proba = clf.predict_proba(X_test)
+      score = scorer(y_test, y_proba[:,1])
+
+      with open(f"open-cc18-gama-ft/{dataset.id}_{fold_nr}.json", "w") as f:
+        fold_res = {
+          "data_id": dataset.id,
+          "name": dataset.name,
+          "metric": scorer_name,
+          "score": score
+        }
+        json.dump(fold_res, f)
+        
+      # clean up
+      if os.path.isdir("cache/"):
+        os.system("rm -rf cache/")
+
